18b780f27aae8f041ed058034d018f4c60f10b94
william.farre@gmail.com <william.farre@gmail.com>
2023-01-25 21:21:11 +0800

    organize css

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const menuBtn = document.getElementById("menu-btn");
 const navbar = document.getElementById("navbar");
 
 menuBtn.addEventListener("click", () => {
-    navbar.classList.toggle("active")
+    navbar.classList.toggle(".active")
 })
